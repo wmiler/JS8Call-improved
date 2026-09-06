7364bfec3fa11fcb5dddcffca91e4c72a3fd8744
Wyatt Miler <wmiler1@gmail.com>
2026-09-06 14:47:10 -0400

    Enhance documentation for CountriesWorked class
    Updated documentation comments for the CountriesWorked class and its methods to provide clearer descriptions and usage details.

--- JS8_Logbook/CountriesWorked.h
+++ JS8_Logbook/CountriesWorked.h
@@ -1,6 +1,6 @@
-/*
- * maintains a list of country names that have been worked
- * VK3ACF July 2013
+/**
+ * @file CountriesWorked.h
+ * @brief Track which DXCC entities (countries) have been worked.
  */
 
 #ifndef __COUNTRIESWORKDED_H
@@ -11,16 +11,48 @@
 #include <QString>
 #include <QStringList>
 
+/**
+ * @class CountriesWorked
+ * @brief Simple boolean set indicating which country names have been worked.
+ *
+ * The class stores a mapping from country name to a boolean flag which is
+ * toggled when a country is marked as worked. It supports initialization
+ * from a list of country names and exposes simple accessors for querying and
+ * counting worked entities.
+ */
 class CountriesWorked {
   public:
+    /**
+     * @brief Initialize the set with a list of known country names.
+     * @param countryNames List of country/entity names to initialize.
+     */
     void init(const QStringList countryNames);
+
+    /**
+     * @brief Mark a country as worked.
+     * @param countryName Name of the country to mark worked.
+     */
     void setAsWorked(const QString countryName);
+
+    /**
+     * @brief Query whether a country has been worked.
+     * @param countryName Name to check.
+     * @return true if worked, false otherwise.
+     */
     bool getHasWorked(const QString countryName) const;
+
+    /**
+     * @brief Count how many countries have been marked as worked.
+     */
     qsizetype getWorkedCount() const;
+
+    /**
+     * @brief Get the total number of tracked country names.
+     */
     qsizetype getSize() const;
 
   private:
-    QHash<QString, bool> _data;
+    QHash<QString, bool> _data; /**< country name -> worked flag */
 };
 
 #endif
